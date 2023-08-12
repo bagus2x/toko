4d7ff2d6a7803797cfcaa0577f84cc2ce212fbc9
Tubagus Saifulloh <dutapanggulu@gmail.com>
2023-08-12 08:56:03 +0700

    Check if Location Services are enabled

--- app/src/main/java/com/bagus2x/toko/ui/storelist/components/RequestLocationPermission.kt
+++ app/src/main/java/com/bagus2x/toko/ui/storelist/components/RequestLocationPermission.kt
@@ -1,5 +1,9 @@
 package com.bagus2x.toko.ui.storelist.components
 
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
+import android.provider.Settings
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,14 +15,22 @@ import androidx.compose.material.ContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.core.location.LocationManagerCompat
 import com.bagus2x.toko.R
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
@@ -26,15 +38,49 @@ fun RequestLocationPermission(
     required: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val locationPermissionsState = rememberMultiplePermissionsState(
-        permissions = listOf(
-            android.Manifest.permission.ACCESS_FINE_LOCATION,
-            android.Manifest.permission.ACCESS_COARSE_LOCATION
+    val context = LocalContext.current
+    var isLocationEnabled by remember { mutableStateOf(context.isLocationEnabled) }
+    LaunchedEffect(context) {
+        while (true) {
+            isLocationEnabled = context.isLocationEnabled
+            delay(1000)
+        }
+    }
+    if (isLocationEnabled && required) {
+        val locationPermissionsState = rememberMultiplePermissionsState(
+            permissions = listOf(
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            )
         )
-    )
 
-    if (locationPermissionsState.allPermissionsGranted && required) {
-        content()
+        if (locationPermissionsState.allPermissionsGranted) {
+            content()
+        } else {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(32.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                val textToShow = if (locationPermissionsState.shouldShowRationale) {
+                    stringResource(R.string.text_location_permission_retionale)
+                } else {
+                    stringResource(R.string.text_location_permission_description)
+                }
+                Text(
+                    text = textToShow,
+                    style = MaterialTheme.typography.body2,
+                    color = MaterialTheme.colors.onBackground.copy(alpha = ContentAlpha.medium),
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                Button(onClick = { locationPermissionsState.launchMultiplePermissionRequest() }) {
+                    Text(stringResource(R.string.text_request_permission))
+                }
+            }
+        }
     } else {
         Column(
             modifier = Modifier
@@ -43,21 +89,27 @@ fun RequestLocationPermission(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            val textToShow = if (locationPermissionsState.shouldShowRationale) {
-                stringResource(R.string.text_location_permission_retionale)
-            } else {
-                stringResource(R.string.text_location_permission_description)
-            }
             Text(
-                text = textToShow,
+                text = stringResource(R.string.text_location_permission_description),
                 style = MaterialTheme.typography.body2,
                 color = MaterialTheme.colors.onBackground.copy(alpha = ContentAlpha.medium),
                 textAlign = TextAlign.Center
             )
-            Spacer(modifier = Modifier.height(32.dp))
-            Button(onClick = { locationPermissionsState.launchMultiplePermissionRequest() }) {
-                Text(stringResource(R.string.text_request_permission))
+
+            Button(
+                onClick = {
+                    val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    context.startActivity(intent)
+                }
+            ) {
+                Text(stringResource(R.string.text_enable_location))
             }
         }
     }
 }
+
+private val Context.isLocationEnabled: Boolean
+    get() {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return LocationManagerCompat.isLocationEnabled(locationManager)
+    }
